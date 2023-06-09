d93039e3520d09d861eda97f35e1501890a51c24
s1300180 <s1300180@std5dc9.u-aizu.ac.jp>
2023-06-09 12:40:33 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -11,6 +11,11 @@ public class DiceGame{
 	System.out.println("Die 1: " + d1);
 	System.out.println("Die 2: " + d2);
 	System.out.println("Total value: " + sum);
-	
+
+	if(sum > 7){
+	    System.out.println("You won!");
+	}else{
+	    System.out.println("You lost!");
+	}
     }
 }
